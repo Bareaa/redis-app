f4ca7d6e7dd5884fe8b08d527dcbe46ea973bdf5
Bareaa <1127375@atitus.edu.br>
2024-02-18 13:26:25 -0300

    correção do teste

--- teste-app.sh
+++ teste-app.sh
@@ -1,8 +1,7 @@
 #/bin/bash
-RESULT="`wget -qO- http://localhost:8090/`"
-wget -q localhost:8090
-if [ $? -eq 0 ]
-then
+RESULT=$(wget -qO- http://localhost:8090/)
+
+if [ $? -eq 0 ]; then
     echo 'ok - serviço no ar!'
 elif [[ $RESULT == *"Number"* ]]; then
     echo 'ok - number of visits'
